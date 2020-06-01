440aafbeeb73834d86d383a495a5301595512779
Soham Dodia <sohamdodia@gmail.com>
2020-06-01 13:47:31 +0530

    Add grpc gateway

--- blog/generate.sh
+++ blog/generate.sh
@@ -1,1 +1,3 @@
 protoc blogpb/blog.proto --go_out=plugins=grpc:.
+
+protoc -I/usr/local/include -I. -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --plugin=protoc-gen-grpc-gateway=$GOPATH/bin/protoc-gen-grpc-gateway  --grpc-gateway_out=logtostderr=true:. ./blog.proto
